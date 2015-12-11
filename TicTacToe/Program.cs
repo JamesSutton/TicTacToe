611a2d2cca6df07e9435c871330ebedd652c4cd5
JamesSutton <james.sutton2@me.com>
2015-12-11 22:25:13 +0000

    Program File

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -12,6 +12,17 @@ namespace TicTacToe
         {
 
             string player1, player2;
+            string A = "1";
+            string B = "2";
+            string C = "3";
+            string D = "4";
+            string E = "5";
+            string F = "6";
+            string G = "7";
+            string H = "8";
+            string I = "9";
+            bool play = true;
+
 
         }
     }
